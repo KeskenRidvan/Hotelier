0d25cb3a802b21006b15079cc7183289dbb56ff3
Ridvan Kesken <kesken.dev@gmail.com>
2024-06-15 23:23:08 +0300

    Admin layout partial view has been split and adjusted.

---
+++ Frontend/Hotelier.WebUI_Asp/Controllers/StaffController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelier.WebUI_Asp.Controllers;
+public class StaffController : Controller
+{
+	public IActionResult Index()
+	{
+		return View();
+	}
+}
