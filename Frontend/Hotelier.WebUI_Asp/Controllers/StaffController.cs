29f91e964fd1474198aaba72b046ee84cb0abbf2
Ridvan Kesken <kesken.dev@gmail.com>
2024-06-17 01:44:41 +0300

    Staffs get api consume were.

--- Frontend/Hotelier.WebUI_Asp/Controllers/StaffController.cs
+++ Frontend/Hotelier.WebUI_Asp/Controllers/StaffController.cs
@@ -1,10 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Hotelier.WebUI_Asp.Models.Staffs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Hotelier.WebUI_Asp.Controllers;
 public class StaffController : Controller
 {
-	public IActionResult Index()
+	private readonly IHttpClientFactory _httpClientFactory;
+
+	public StaffController(IHttpClientFactory httpClientFactory)
 	{
-		return View();
+		_httpClientFactory = httpClientFactory;
+	}
+
+	public async Task<IActionResult> Index()
+	{
+		var client = _httpClientFactory.CreateClient();
+		var responseMessage = await client.GetAsync("https://localhost:7094/api/staffs");
+
+		if (!responseMessage.IsSuccessStatusCode)
+			return View();
+
+		var jsonData = await responseMessage.Content.ReadAsStringAsync();
+		var values = JsonConvert.DeserializeObject<List<StaffViewModel>>(jsonData);
+		return View(values);
 	}
 }
---
+++ Frontend/Hotelier.WebUI_Asp/Models/Staffs/StaffViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Hotelier.WebUI_Asp.Models.Staffs;
+
+public class StaffViewModel
+{
+	public int StaffID { get; set; }
+	public string Name { get; set; }
+	public string Title { get; set; }
+}
