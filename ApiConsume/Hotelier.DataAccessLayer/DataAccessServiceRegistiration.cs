1aa7cff30a38e28e90bf4f5e51eda64547537e6f
Ridvan Kesken <kesken.dev@gmail.com>
2024-06-23 21:19:40 +0300

    fix: Models ServiceRegistirations have been added.

--- ApiConsume/Hotelier.DataAccessLayer/DataAccessServiceRegistiration.cs
+++ ApiConsume/Hotelier.DataAccessLayer/DataAccessServiceRegistiration.cs
@@ -7,18 +7,26 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Hotelier.DataAccessLayer;
 public static class DataAccessServiceRegistiration
 {
-	public static IServiceCollection AddDataAccessServices(
-		this IServiceCollection services,
-		IConfiguration configuration)
-	{
-		services.AddDbContext<BaseDbContext>();
+    public static IServiceCollection AddDataAccessServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<BaseDbContext>();
 
-		services.AddScoped<IRoomDal, RoomDal>();
-		services.AddScoped<IServiceDal, ServiceDal>();
-		services.AddScoped<IStaffDal, StaffDal>();
-		services.AddScoped<ISubscribeDal, SubscribeDal>();
-		services.AddScoped<ITestimonialDal, TestimonialDal>();
+        services.AddScoped<IAboutDal, AboutDal>();
+        services.AddScoped<IAppUserDal, AppUserDal>();
+        services.AddScoped<IBookingDal, BookingDal>();
+        services.AddScoped<IContactDal, ContactDal>();
+        services.AddScoped<IGuestDal, GuestDal>();
+        services.AddScoped<IMessageCategoryDal, MessageCategoryDal>();
+        services.AddScoped<IRoomDal, RoomDal>();
+        services.AddScoped<ISendMessageDal, SendMessageDal>();
+        services.AddScoped<IServiceDal, ServiceDal>();
+        services.AddScoped<IStaffDal, StaffDal>();
+        services.AddScoped<ISubscribeDal, SubscribeDal>();
+        services.AddScoped<ITestimonialDal, TestimonialDal>();
+        services.AddScoped<IWorkLocationDal, WorkLocationDal>();
 
-		return services;
-	}
+        return services;
+    }
 }
