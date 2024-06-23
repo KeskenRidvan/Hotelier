b9a6e607a6f9fadfa6cc28a2514d8fc74ba1128f
Ridvan Kesken <kesken.dev@gmail.com>
2024-06-23 16:15:04 +0300

    fix: Models ServiceRegistirations have been added.

--- ApiConsume/Hotelier.BusinessLayer/BusinessServiceRegistiration.cs
+++ ApiConsume/Hotelier.BusinessLayer/BusinessServiceRegistiration.cs
@@ -6,17 +6,25 @@ using System.Reflection;
 namespace Hotelier.BusinessLayer;
 public static class BusinessServiceRegistiration
 {
-	public static IServiceCollection AddBusinessServices(
-		this IServiceCollection services)
-	{
-		services.AddScoped<IRoomService, RoomManager>();
-		services.AddScoped<IServiceService, ServiceManager>();
-		services.AddScoped<IStaffService, StaffManager>();
-		services.AddScoped<ISubscribeService, SubscribeManager>();
-		services.AddScoped<ITestimonialService, TestimonialManager>();
+    public static IServiceCollection AddBusinessServices(
+        this IServiceCollection services)
+    {
 
+        services.AddScoped<IAboutService, AboutManager>();
+        services.AddScoped<IAppUserService, AppUserManager>();
+        services.AddScoped<IBookingService, BookingManager>();
+        services.AddScoped<IContactService, ContactManager>();
+        services.AddScoped<IGuestService, GuestManager>();
+        services.AddScoped<IMessageCategoryService, MessageCategoryManager>();
+        services.AddScoped<IRoomService, RoomManager>();
+        services.AddScoped<ISendMessageService, SendMessageManager>();
+        services.AddScoped<IServiceService, ServiceManager>();
+        services.AddScoped<IStaffService, StaffManager>();
+        services.AddScoped<ISubscribeService, SubscribeManager>();
+        services.AddScoped<ITestimonialService, TestimonialManager>();
+        services.AddScoped<IWorkLocationService, WorkLocationManager>();
 
-		services.AddAutoMapper(Assembly.GetExecutingAssembly());
-		return services;
-	}
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        return services;
+    }
 }
