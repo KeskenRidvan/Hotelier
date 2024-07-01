8f05bad1b0ad56b408619e11e671e169d81e3461
Ridvan Kesken <kesken.dev@gmail.com>
2024-07-02 01:23:27 +0300

    Mail operations were carried out on admin pages.

---
+++ Frontend/Hotelier.WebUI_Asp/Controllers/AdminMailsController.cs
@@ -0,0 +1,43 @@
+﻿using Hotelier.WebUI_Asp.Models.Mails;
+using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+
+namespace Hotelier.WebUI_Asp.Controllers;
+public class AdminMailsController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Index(AdminMailViewModel model)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+
+        MailboxAddress mailboxAddressFrom = new MailboxAddress("HotelierAdmin", "birmailadresi@gmail.com");
+        mimeMessage.From.Add(mailboxAddressFrom);
+
+        MailboxAddress mailboxAddressTo = new MailboxAddress("User", model.ReceiverMail);
+        mimeMessage.To.Add(mailboxAddressTo);
+
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = model.Body;
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+
+        mimeMessage.Subject = model.Subject;
+
+        SmtpClient client = new SmtpClient();
+        client.Connect("smtp.gmail.com", 587, false);
+        client.Authenticate("birmailadresi@gmail.com", "mail adresi password key");
+        client.Send(mimeMessage);
+        client.Disconnect(true);
+
+        //Gönderilen Mailin Veri Tabanına Kaydedilmesi.
+
+
+        return View();
+    }
+}
---
+++ Frontend/Hotelier.WebUI_Asp/Models/Mails/AdminMailViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hotelier.WebUI_Asp.Models.Mails;
+
+public class AdminMailViewModel
+{
+    public string Name { get; set; }
+    public string SenderMail { get; set; }
+    public string ReceiverMail { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+}
