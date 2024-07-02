024ffdcfc1b549fe2e6cc00a0be62de73247a6cb
Ridvan Kesken <kesken.dev@gmail.com>
2024-07-02 14:40:38 +0300

    Image upload and file upload operations have been defined on the admin side.

---
+++ ApiConsume/Hotelier.WebApi/Controllers/FileProcessesController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotelier.WebApi.Controllers;
+[Route("api/fileprocesses")]
+[ApiController]
+public class FileProcessesController : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> UploadFile([FromForm] IFormFile file)
+    {
+        var fileName = Guid.NewGuid() + Path.GetExtension(file.FileName);
+        var path = Path.Combine(Directory.GetCurrentDirectory(), "files/" + fileName);
+        var stream = new FileStream(path, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return Created("", file);
+    }
+}
---
+++ Frontend/Hotelier.WebUI_Asp/Controllers/AdminImageFilesController.cs
@@ -0,0 +1,32 @@
+﻿using Hotelier.WebUI_Asp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace Hotelier.WebUI_Asp.Controllers;
+public class AdminImageFilesController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Index(IFormFile file)
+    {
+        var stream = new MemoryStream();
+        await file.CopyToAsync(stream);
+        var bytes = stream.ToArray();
+
+        ByteArrayContent byteArrayContent = new ByteArrayContent(bytes);
+        byteArrayContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+
+        MultipartFormDataContent multipartFormDataContent = new();
+        multipartFormDataContent.Add(byteArrayContent, "file", file.FileName);
+
+        var httpclient = new HttpClient();
+        await httpclient.PostAsync($"{Constants.BaseUrl}/fileimages", multipartFormDataContent);
+
+        return View();
+    }
+}
